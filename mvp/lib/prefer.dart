91ca98069ef9547d54dc56863f3cd7eb5d92f559
madol <dingkuoldit@gmail>
2025-03-15 11:11:23 +0300

    image,button&expanded widgets

---
+++ mvp/lib/prefer.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class CoffeePrefs extends StatelessWidget {
+  const CoffeePrefs({super.key});
+
+  void incrementStrength() {
+    print('inc strength by 1');
+  }
+  void incrementSugars() {
+    print('inc sugars by 1');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        Row(
+          children: [
+           const Text('strength: ', ),
+            const Text('3'),
+            Image.asset('assets/img/coffee_bean.png',
+            width: 25,
+            color: Colors.brown[100],
+            colorBlendMode: BlendMode.multiply,
+            ),
+         const Expanded(child: SizedBox()), 
+         FilledButton(
+          style: FilledButton.styleFrom(
+            backgroundColor:Colors.brown,
+            foregroundColor:Colors.white,
+          ),
+            onPressed: incrementStrength,
+            child:const Text('+'),
+          ),
+          ],
+        ),
+        Row(
+          children: [
+           const Text('sugar: ', ),
+           const Text('2'),
+            Image.asset('assets/img/sugar_cube.png',
+            width: 25,
+            color: Colors.brown[100],
+            colorBlendMode: BlendMode.multiply,
+            ),
+          const Expanded(child: SizedBox()),
+          
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.brown,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: incrementSugars,
+            child:const Text('+'),
+          ),
+          ],
+        ),
+      ],
+    );
+  }
+}//----
